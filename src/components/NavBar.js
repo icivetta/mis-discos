684ca144220107fd102d9aee93a39b9859fcc106
Ignacio Civetta <54008625+icivetta@users.noreply.github.com>
2022-02-27 17:45:41 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+function NavBar(){
+    return(
+        <div className='nav-bar'>
+            
+                <ul>
+                    <li><a href="">Mis Discos</a></li>
+                    <li><a href="">Quienes somos</a></li>
+                    <li><a href="">Locales</a></li>
+                    <li><a href="">Internacionales</a></li>
+                </ul>
+            
+
+        </div>
+    )
+}
+export default NavBar
